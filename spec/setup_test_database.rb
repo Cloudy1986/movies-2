57f2cf910e8df5e774065f279f0eaae5d3f3b349
Mark McLeod <markmcleod45@gmail.com>
2022-02-08 15:51:28 +0000

    Truncate comments table before each test runs

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,5 +3,5 @@ require 'pg'
 def setup_test_database
   connection = PG.connect(dbname: 'movie_database_test')
   p "Setting up test database"
-  connection.exec("TRUNCATE movies;")
+  connection.exec("TRUNCATE movies, comments;")
 end
