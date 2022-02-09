0ea61421719f20d4b42f00a4ba0a5c9d2bcad349
Mark McLeod <markmcleod45@gmail.com>
2022-02-09 16:07:58 +0000

    Refactor to add some navigation

--- spec/features/user_can_add_and_view_comment_spec.rb
+++ spec/features/user_can_add_and_view_comment_spec.rb
@@ -13,4 +13,19 @@ feature 'Add and View comments' do
     expect(page).to have_content 'The Power of the Dog'
     expect(page).to have_content 'This film is going to win some Oscars'
   end
+
+  scenario 'user can navigate to comments' do
+    movie = Movie.create(title: 'The Daughter')
+    Comment.create(text: 'This is a comment on this film' , movie_id: movie.id)
+    Comment.create(text: 'This is another comment on this film' , movie_id: movie.id)
+    
+    visit '/movies'
+    expect(page).to have_content 'Movie List'
+    expect(page).to have_content 'The Daughter'
+    click_button 'View comments'
+    expect(current_path).to eq "/movies/#{movie.id}/comments"
+    expect(page).to have_content 'The Daughter'
+    expect(page).to have_content 'This is a comment on this film'
+    expect(page).to have_content 'This is another comment on this film'
+  end
 end
